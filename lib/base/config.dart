f951b049d0e8886fc3d52b349846f5f231f6295e
HyeongJooPark <hyeongjoopark0324@gmail.com>
2023-05-23 17:32:06 +0900

    config 수정

--- lib/base/config.dart
+++
@@ -1,8 +0,0 @@
-class Config {
-  static const String LOCAL = "";
-  static const String DEV = "";
-  static const String API_VERSION = "/api/v1";
-  static const String BASE_URL = DEV + API_VERSION;
-
-  static const String KAKAO_NATIVE_APP_KEY = "";
-}
